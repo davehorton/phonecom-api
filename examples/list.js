d896d1cfd8747c65fc9e086188a065f5a85430ee
Dave Horton <daveh@beachdognet.com>
2017-03-03 16:16:45 -0500

    added support for parameters in http requests

--- examples/list.js
+++ examples/list.js
@@ -12,25 +12,25 @@ let api = new ApiClient(argv.token) ;
 
 let fn ;
 switch( argv.objectType ) {
-  case 'Accounts': fn = api.listAccounts.bind(api) ; break ;
-  case 'Extensions': fn = api.listExtensions.bind(api, argv.accountId); break ;
-  case 'Applications': fn = api.listApplications.bind(api, argv.accountId); break ;
-  case 'CallLogs': fn = api.listCallLogs.bind(api, argv.accountId); break ;
-  case 'Devices': fn = api.listDevices.bind(api, argv.accountId); break ;
-  case 'Listeners': fn = api.listListeners.bind(api, argv.accountId); break ;
-  case 'ExpressServiceCodes': fn = api.listExpressServiceCodes.bind(api, argv.accountId); break ;
-  case 'CallerIds': fn = api.listCallerIds.bind(api, argv.accountId, argv.extensionId); break ;
-  case 'Contacts': fn = api.listContacts.bind(api, argv.accountId, argv.extensionId); break ;
-  case 'Groups': fn = api.listGroups.bind(api, argv.accountId, argv.extensionId); break ;
-  case 'Media': fn = api.listMedia.bind(api, argv.accountId); break ;
-  case 'Menus': fn = api.listMenus.bind(api, argv.accountId); break ;
-  case 'PhoneNumbers': fn = api.listPhoneNumbers.bind(api, argv.accountId); break ;
-  case 'Routes': fn = api.listRoutes.bind(api, argv.accountId); break ;
-  case 'Queues': fn = api.listQueues.bind(api, argv.accountId); break ;
-  case 'Schedules': fn = api.listSchedules.bind(api, argv.accountId); break ;
-  case 'Sms': fn = api.listSms.bind(api, argv.accountId); break ;
-  case 'Subaccounts': fn = api.listSubaccounts.bind(api, argv.accountId); break ;
-  case 'Trunks': fn = api.listTrunks.bind(api, argv.accountId); break ;
+  case 'Accounts': fn = api.listAccounts.bind(api, {fields: 'brief'}) ; break ;
+  case 'Extensions': fn = api.listExtensions.bind(api, argv.accountId, {fields: 'brief'}); break ;
+  case 'Applications': fn = api.listApplications.bind(api, argv.accountId, {fields: 'brief'}); break ;
+  case 'CallLogs': fn = api.listCallLogs.bind(api, argv.accountId, {fields: 'brief'}); break ;
+  case 'Devices': fn = api.listDevices.bind(api, argv.accountId, {fields: 'brief'}); break ;
+  case 'Listeners': fn = api.listListeners.bind(api, argv.accountId, {fields: 'brief'}); break ;
+  case 'ExpressServiceCodes': fn = api.listExpressServiceCodes.bind(api, argv.accountId, {fields: 'brief'}); break ;
+  case 'CallerIds': fn = api.listCallerIds.bind(api, argv.accountId, argv.extensionId, {fields: 'brief'}); break ;
+  case 'Contacts': fn = api.listContacts.bind(api, argv.accountId, argv.extensionId, {fields: 'brief'}); break ;
+  case 'Groups': fn = api.listGroups.bind(api, argv.accountId, argv.extensionId, {fields: 'brief'}); break ;
+  case 'Media': fn = api.listMedia.bind(api, argv.accountId, {fields: 'brief'}); break ;
+  case 'Menus': fn = api.listMenus.bind(api, argv.accountId, {fields: 'brief'}); break ;
+  case 'PhoneNumbers': fn = api.listPhoneNumbers.bind(api, argv.accountId, {fields: 'brief'}); break ;
+  case 'Routes': fn = api.listRoutes.bind(api, argv.accountId, {fields: 'brief'}); break ;
+  case 'Queues': fn = api.listQueues.bind(api, argv.accountId, {fields: 'brief'}); break ;
+  case 'Schedules': fn = api.listSchedules.bind(api, argv.accountId, {fields: 'brief'}); break ;
+  case 'Sms': fn = api.listSms.bind(api, argv.accountId, {fields: 'brief'}); break ;
+  case 'Subaccounts': fn = api.listSubaccounts.bind(api, argv.accountId, {fields: 'brief'}); break ;
+  case 'Trunks': fn = api.listTrunks.bind(api, argv.accountId, {fields: 'brief'}); break ;
 
 
   default:
